03d647cb4265a939eaa4ca6aa0818172048f53a8
sagar460 <shresthasagr7000@gmail.com>
2025-03-07 13:09:56 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "This is App Page ",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.json({
+    message: "This is About Page",
+  });
+});
+
+app.listen(2000, () => {
+  console.log("Nodejs project has been started");
+});
